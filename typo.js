82874ad3ea0c52eb450f9cf990180740e60dffd8
Matt Flowers <flattmo@gmail.com>
2016-07-17 10:58:25 -0400

    Added UMD

--- typo.js
+++ typo.js
@@ -1,4 +1,21 @@
-module.exports = (function() {
+https://github.com/umdjs/umd/blob/master/templates/returnExports.js
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define([], factory);
+    } else if (typeof module === 'object' && module.exports) {
+        // Node. Does not work with strict CommonJS, but
+        // only CommonJS-like environments that support module.exports,
+        // like Node.
+        module.exports = factory();
+    } else {
+        // Browser globals (root is window)
+        root.returnExports = factory();
+  }
+}(this, function () {
+
+    'use strict';
+
     function typo(input) {
         if (input.length <= 1)
             return input;
@@ -21,6 +38,6 @@ module.exports = (function() {
     }
 
     var punctuation = /["(){}[\]<>/\\|`'*!@#$%^&\-_=+:;,.? ]/;
-
-    return typo;
-}());
+    
+    return {typo:typo};
+}));
